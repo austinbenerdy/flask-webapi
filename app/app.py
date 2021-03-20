6e429234708d12524fda7a4d8d0d6a276b2893d3
Austin Adamson <austin.l.adamson@gmail.com>
2021-03-19 19:26:36 -0700

    Adding sqlalchemy

--- app/app.py
+++ app/app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+
+    return 'Hello World!'
 
 
 @app.route('/hello')
---
+++ app/init.py
@@ -0,0 +1,10 @@
+# from flask import Flask
+# import flask_sqlalchemy
+#
+# import mariadb
+# import sys
+#
+# try:
+#     conn = mariadb.connect()
+# except mariadb.Error as e:
+#     sys.exit(4)
