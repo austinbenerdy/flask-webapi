9ef3ce388c2e0b461d1aed416c037343e21cda26
Austin Adamson <austin.l.adamson@gmail.com>
2021-03-31 20:52:01 -0700

    Fixing database migrations!

---
+++ app/alembic/versions/72534dacc9da_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: 72534dacc9da
+Revises: 
+Create Date: 2021-04-01 03:36:56.623330
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '72534dacc9da'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
